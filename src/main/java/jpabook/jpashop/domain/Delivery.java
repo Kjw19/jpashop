dfa74e0143fcc1357700f8f5a8a5ae15e6f2597c
김재우 <63536145+Kjw19@users.noreply.github.com>
2023-05-29 22:31:03 +0900

    Jpa 활용2편을 위한 커밋

---
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,28 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.persistence.criteria.Fetch;
+import static javax.persistence.FetchType.*;
+
+@Entity
+@Getter
+@Setter
+public class Delivery {
+    @Id
+    @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(fetch = LAZY)
+    @JoinColumn(name = "delivery")
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING) //ORDINAL 사용 x
+    private DeliveryStatus status; //Ready, COMP
+}
